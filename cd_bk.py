16a1acee308b5f1fa85350a63140e65c681b9e0f
Andrey Kvichanskiy <kvichans@mail.ru>
2017-03-15 12:19:28 +0300

    fix #23

--- cd_bk.py
+++ cd_bk.py
@@ -372,7 +372,7 @@ class Command:
         vrn_data= load_cfg(ops='vrn_data')
         if  not vrn_data: return
         sv_path = get_bk_path(cf_path, vrn_data['whon'], vrn_data['maon'])
-        pass;                   LOG and log('sv_path={}',(sv_path))
+        pass;                  #LOG and log('sv_path={}',(sv_path))
         sv_dir, \
         sv_fn   = os.path.split(sv_path)
         if not os.path.isdir(sv_dir):
